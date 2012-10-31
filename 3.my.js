bd77324b53982a8588f29b4a8a7c96e0c45b7ab4
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2012-11-01 03:20:13 +0700

    added more files

---
+++ 3.my.js
@@ -0,0 +1,10 @@
+var timers = require('./timers_my.3.js');
+var setTimeout = timers.setTimeout;
+var clearTimeout = timers.clearTimeout;
+
+var noop = function() {};
+setTimeout(noop, 1); // in order to preserve linked list not empty
+for(var i = 0; i < 1000000; i++) {
+  var t = setTimeout(noop, 1);
+  clearTimeout(t);
+}

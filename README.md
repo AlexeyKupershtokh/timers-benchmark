15815f1fa217d8db75afe742d125ecafeb0c7c5b
wicked <wicked@alawar.com>
2012-11-01 12:32:00 +0700

    using benchmark.js

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
 timers-benchmark
 ================
-Run:
 ```
-time node 1.js
-time node 2.js
-time node 3.js
-```
-Strace:
-```
-time strace node 1.js
-time strace node 2.js
-time strace node 3.js
-```
-Diff:
-```
-diff 1.js 2.js
-diff 1.js 3.js
+git clone ...
+npm install
+node run.js
 ```
---
+++ run.js
@@ -0,0 +1,58 @@
+var Benchmark = require('benchmark').Benchmark;
+
+timer_modules = {
+  "0.8.12" : {
+    "_ = null": require('./timers_0.8.12.1.js'),
+    "delete _; _ = null": require('./timers_0.8.12.2.js'),
+    "keep _": require('./timers_0.8.12.3.js'),
+  },
+  "0.9.3" : {
+    "_ = null": require('./timers_0.9.3.1.js'),
+    "delete _; _ = null": require('./timers_0.9.3.2.js'),
+    "keep _": require('./timers_0.8.12.3.js'),
+  },
+  "0.9.3 optimized" : {
+    "_ = null": require('./timers_my.1.js'),
+    "delete _; _ = null": require('./timers_my.2.js'),
+    "keep _": require('./timers_my.3.js'),
+  }
+}
+
+noop = function() {};
+
+var suite = new Benchmark.Suite;
+
+// add tests
+suite.add('warmup', function() {
+  var d = Date.now();
+});
+for (var v in timer_modules) {
+  for (patch in timer_modules[v]) {
+    var name = v + '; ' + patch;
+    suite.add(v + '; ' + patch, '\
+      var setTimeout2   = timer_modules[\'' + v + '\'][\'' + patch + '\'].setTimeout; \
+      var clearTimeout2 = timer_modules[\'' + v + '\'][\'' + patch + '\'].clearTimeout; \
+      var y = setTimeout2(noop, 1); \
+      for(var i = 0; i < 1000; i++) { \
+        var t = setTimeout2(noop, 1); \
+        clearTimeout2(t); \
+      } \
+      clearTimeout2(y); \
+    ');
+  }
+}
+// add listeners
+suite.on('setup', function() {
+  console.log('setup');
+});
+suite.on('cycle', function(event) {
+  while(event.target.name.length < 35) { event.target.name += ' '; }
+  console.log(String(event.target));
+})
+suite.on('error', function(event) {
+  console.log(event.target.error);
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+})
+.run();
